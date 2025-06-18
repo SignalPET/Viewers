5ff22f32d4cc671b61fd4f1b400e40739483834a
harel.chen <harel.chen@signalpet.com>
2025-06-18 16:02:33 +0300

    init

---
+++ extensions/signalpet-ohif-ui/src/components/CustomThumbnail.tsx
@@ -0,0 +1,347 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { useDrag } from 'react-dnd';
+import { Icons } from '@ohif/ui-next';
+import { DisplaySetMessageListTooltip } from '@ohif/ui-next';
+import { TooltipTrigger, TooltipContent, Tooltip } from '@ohif/ui-next';
+import { useSystem } from '@ohif/core';
+import moment from 'moment';
+
+export interface CustomThumbnailProps {
+  displaySetInstanceUID: string;
+  className?: string;
+  imageSrc?: string;
+  imageAltText?: string;
+  description: string;
+  seriesNumber?: any;
+  numInstances: number;
+  loadingProgress?: number;
+  countIcon?: string;
+  messages?: any;
+  isActive: boolean;
+  onClick: (event: any) => void;
+  onDoubleClick: (event: any) => void;
+  thumbnailType?: string;
+  modality?: string;
+  viewPreset?: string;
+  isHydratedForDerivedDisplaySet?: boolean;
+  isTracked?: boolean;
+  canReject?: boolean;
+  dragData?: any;
+  onReject?: () => void;
+  onClickUntrack?: () => void;
+  ThumbnailMenuItems?: React.ComponentType<any>;
+}
+
+/**
+ * SignalPET Custom Thumbnail Component
+ * Based on OHIF Thumbnail with SignalPET-specific modifications
+ */
+const CustomThumbnail: React.FC<CustomThumbnailProps> = ({
+  displaySetInstanceUID,
+  className,
+  imageSrc,
+  imageAltText,
+  description,
+  seriesNumber,
+  numInstances,
+  loadingProgress,
+  countIcon,
+  messages,
+  isActive,
+  onClick,
+  onDoubleClick,
+  thumbnailType,
+  modality,
+  viewPreset = 'thumbnails',
+  isHydratedForDerivedDisplaySet = false,
+  isTracked = false,
+  canReject = false,
+  dragData = {},
+  onReject = () => {},
+  onClickUntrack = () => {},
+  ThumbnailMenuItems = () => null,
+}) => {
+  // Access DisplaySetService via useSystem hook
+  const { servicesManager } = useSystem();
+  const displaySetService = servicesManager?.services?.displaySetService;
+
+  const displaySet = displaySetService?.getDisplaySetByUID(displaySetInstanceUID);
+  const instance = displaySet?.instances?.[0];
+
+  const [collectedProps, drag, dragPreview] = useDrag({
+    type: 'displayset',
+    item: { ...dragData },
+    canDrag: function (monitor) {
+      return Object.keys(dragData).length !== 0;
+    },
+  });
+
+  const [lastTap, setLastTap] = useState(0);
+
+  const handleTouchEnd = (e: any) => {
+    const currentTime = new Date().getTime();
+    const tapLength = currentTime - lastTap;
+    if (tapLength < 300 && tapLength > 0) {
+      onDoubleClick(e);
+    } else {
+      onClick(e);
+    }
+    setLastTap(currentTime);
+  };
+
+  function formatDicomDateTime(instance: any): string {
+    const date = instance?.InstanceCreationDate || instance?.AcquisitionDate;
+    const time = instance?.InstanceCreationTime || instance?.AcquisitionTime;
+    if (!date) return '';
+    // DICOM time can be HHmmss, HHmm, or HH
+    let formatted = moment(date, 'YYYYMMDD');
+    if (time) {
+      // Pad time to at least 6 digits
+      const paddedTime = time.padEnd(6, '0');
+      formatted = moment(date + paddedTime, 'YYYYMMDDHHmmss');
+    }
+    return formatted.format('MMM, D, YYYY hh:mma');
+  }
+
+  const renderThumbnailPreset = () => {
+    return (
+      <div
+        className={classnames(
+          'flex h-full w-full flex-col items-center justify-center gap-[2px] p-[4px]',
+          isActive && 'bg-popover rounded'
+        )}
+      >
+        <div className="h-[114px] w-[128px]">
+          <div className="relative bg-black">
+            {imageSrc ? (
+              <img
+                src={imageSrc}
+                alt={imageAltText || ''}
+                className="h-[114px] w-[128px] rounded object-contain"
+                crossOrigin="anonymous"
+              />
+            ) : (
+              <div className="bg-background h-[114px] w-[128px] rounded"></div>
+            )}
+
+            {/* bottom left */}
+            <div className="absolute bottom-0 left-0 flex h-[14px] items-center gap-[4px] rounded-tr pt-[10px] pb-[10px] pr-[6px] pl-[5px]">
+              <div
+                className={classnames(
+                  'h-[10px] w-[10px] rounded-[2px]',
+                  isActive || isHydratedForDerivedDisplaySet ? 'bg-highlight' : 'bg-primary/65',
+                  loadingProgress && loadingProgress < 1 && 'bg-primary/25'
+                )}
+              ></div>
+              <div className="text-[11px] font-semibold text-white">{modality}</div>
+            </div>
+
+            {/* top right */}
+            <div className="absolute top-0 right-0 flex items-center gap-[4px]">
+              <DisplaySetMessageListTooltip
+                messages={messages}
+                id={`display-set-tooltip-${displaySetInstanceUID}`}
+              />
+              {isTracked && (
+                <Tooltip>
+                  <TooltipTrigger>
+                    <div className="group">
+                      <Icons.StatusTracking className="text-primary-light h-[15px] w-[15px] group-hover:hidden" />
+                      <Icons.Cancel
+                        className="text-primary-light hidden h-[15px] w-[15px] group-hover:block"
+                        onClick={onClickUntrack}
+                      />
+                    </div>
+                  </TooltipTrigger>
+                  <TooltipContent side="right">
+                    <div className="flex flex-1 flex-row">
+                      <div className="flex-2 flex items-center justify-center pr-4">
+                        <Icons.InfoLink className="text-primary" />
+                      </div>
+                      <div className="flex flex-1 flex-col">
+                        <span>
+                          <span className="text-white">
+                            {isTracked ? 'Series is tracked' : 'Series is untracked'}
+                          </span>
+                        </span>
+                      </div>
+                    </div>
+                  </TooltipContent>
+                </Tooltip>
+              )}
+            </div>
+            {/* bottom right */}
+            <div className="absolute bottom-0 right-0 flex items-center gap-[4px] p-[4px]">
+              {ThumbnailMenuItems && (
+                <ThumbnailMenuItems
+                  displaySetInstanceUID={displaySetInstanceUID}
+                  canReject={canReject}
+                  onReject={onReject}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="flex h-[52px] w-[128px] flex-col justify-start pt-px">
+          <Tooltip>
+            <TooltipContent>{description}</TooltipContent>
+            <TooltipTrigger>
+              <div className="min-h-[18px] w-[128px] overflow-hidden text-ellipsis whitespace-nowrap pb-0.5 pl-1 text-left text-[12px] font-normal leading-4 text-white">
+                {description}
+              </div>
+            </TooltipTrigger>
+          </Tooltip>
+          <div className="flex h-[12px] items-center gap-[7px] overflow-hidden">
+            <div className="text-muted-foreground text-center text-[11px]">
+              {formatDicomDateTime(instance)}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const renderListPreset = () => {
+    return (
+      <div
+        className={classnames(
+          'flex h-full w-full items-center justify-between pr-[8px] pl-[8px] pt-[4px] pb-[4px]',
+          isActive && 'bg-popover rounded'
+        )}
+      >
+        <div className="relative flex h-[32px] w-full items-center gap-[8px] overflow-hidden">
+          <div
+            className={classnames(
+              'h-[32px] w-[4px] min-w-[4px] rounded',
+              isActive || isHydratedForDerivedDisplaySet ? 'bg-highlight' : 'bg-primary/65',
+              loadingProgress && loadingProgress < 1 && 'bg-primary/25'
+            )}
+          ></div>
+          <div className="flex h-full w-[calc(100%-12px)] flex-col justify-start">
+            <div className="flex items-center gap-[7px]">
+              <div className="text-[13px] font-semibold text-white">{modality}</div>
+              <Tooltip>
+                <TooltipContent>{description}</TooltipContent>
+                <TooltipTrigger className="w-full overflow-hidden">
+                  <div className="max-w-[160px] overflow-hidden overflow-ellipsis whitespace-nowrap text-left text-[13px] font-normal text-white">
+                    {description}
+                  </div>
+                </TooltipTrigger>
+              </Tooltip>
+            </div>
+
+            <div className="flex h-[12px] items-center gap-[7px] overflow-hidden">
+              <div className="text-muted-foreground text-center text-[11px]">
+                {formatDicomDateTime(instance)}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex h-full items-center gap-[4px]">
+          <DisplaySetMessageListTooltip
+            messages={messages}
+            id={`display-set-tooltip-${displaySetInstanceUID}`}
+          />
+          {isTracked && (
+            <Tooltip>
+              <TooltipTrigger>
+                <div className="group">
+                  <Icons.StatusTracking className="text-primary-light h-[20px] w-[15px] group-hover:hidden" />
+                  <Icons.Cancel
+                    className="text-primary-light hidden h-[15px] w-[15px] group-hover:block"
+                    onClick={onClickUntrack}
+                  />
+                </div>
+              </TooltipTrigger>
+              <TooltipContent side="right">
+                <div className="flex flex-1 flex-row">
+                  <div className="flex-2 flex items-center justify-center pr-4">
+                    <Icons.InfoLink className="text-primary" />
+                  </div>
+                  <div className="flex flex-1 flex-col">
+                    <span>
+                      <span className="text-white">
+                        {isTracked ? 'Series is tracked' : 'Series is untracked'}
+                      </span>
+                    </span>
+                  </div>
+                </div>
+              </TooltipContent>
+            </Tooltip>
+          )}
+          {ThumbnailMenuItems && (
+            <ThumbnailMenuItems
+              displaySetInstanceUID={displaySetInstanceUID}
+              canReject={canReject}
+              onReject={onReject}
+            />
+          )}
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div
+      className={classnames(
+        className,
+        'bg-muted hover:bg-primary/30 group flex cursor-pointer select-none flex-col rounded outline-none',
+        viewPreset === 'thumbnails' && 'h-[170px] w-[135px]',
+        viewPreset === 'list' && 'h-[40px] w-full'
+      )}
+      id={`thumbnail-${displaySetInstanceUID}`}
+      data-cy={
+        thumbnailType === 'thumbnailNoImage'
+          ? 'study-browser-thumbnail-no-image'
+          : 'study-browser-thumbnail'
+      }
+      data-series={seriesNumber}
+      onClick={onClick}
+      onDoubleClick={onDoubleClick}
+      onTouchEnd={handleTouchEnd}
+      role="button"
+    >
+      <div ref={drag} className="h-full w-full">
+        {viewPreset === 'thumbnails' && renderThumbnailPreset()}
+        {viewPreset === 'list' && renderListPreset()}
+      </div>
+    </div>
+  );
+};
+
+CustomThumbnail.propTypes = {
+  displaySetInstanceUID: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  imageSrc: PropTypes.string,
+  /**
+   * Data the thumbnail should expose to a receiving drop target. Use a matching
+   * `dragData.type` to identify which targets can receive this draggable item.
+   * If this is not set, drag-n-drop will be disabled for this thumbnail.
+   *
+   * Ref: https://react-dnd.github.io/react-dnd/docs/api/use-drag#specification-object-members
+   */
+  dragData: PropTypes.shape({
+    /** Must match the "type" a dropTarget expects */
+    type: PropTypes.string.isRequired,
+  }),
+  imageAltText: PropTypes.string,
+  description: PropTypes.string.isRequired,
+  seriesNumber: PropTypes.any,
+  numInstances: PropTypes.number.isRequired,
+  loadingProgress: PropTypes.number,
+  messages: PropTypes.object,
+  isActive: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+  onDoubleClick: PropTypes.func.isRequired,
+  viewPreset: PropTypes.string,
+  modality: PropTypes.string,
+  isHydratedForDerivedDisplaySet: PropTypes.bool,
+  isTracked: PropTypes.bool,
+  onClickUntrack: PropTypes.func,
+  countIcon: PropTypes.string,
+  thumbnailType: PropTypes.oneOf(['thumbnail', 'thumbnailTracked', 'thumbnailNoImage']),
+};
+
+export default CustomThumbnail;
