f938411d092c0537a7f19de59f13cd43f45fe0ff
liad <liad.mordekoviz@signalpet.com>
2025-07-06 12:49:38 +0300

    fix(DICOMWeb): Improve null check for ModalitiesInStudy.Value property

--- platform/core/src/DICOMWeb/getModalities.js
+++ platform/core/src/DICOMWeb/getModalities.js
@@ -1,5 +1,5 @@
 export default function getModalities(Modality, ModalitiesInStudy) {
-  if (!Modality && !ModalitiesInStudy) {
+  if (!Modality && (!ModalitiesInStudy || !ModalitiesInStudy.Value)) {
     return {};
   }
 
